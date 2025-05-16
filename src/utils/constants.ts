c1a35daba7cbb51edb8dd492fcf1979b8594f929
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-05-16 21:44:34 +0500

    UPDATED: Upload render db url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,7 +6,7 @@ export const PORT_NUMBER = process.env.PORT || 7000;
 export const MORGRAN_FORMAT = ":remote-addr - :remote-user :method :url :status :response-time ms";
 export const CPUS_COUNT = os.cpus().length;
 
-export const WHITE_LIST_DOMAINS: string[] = ['http://localhost:5173', 'http://localhost:4173'];
+export const WHITE_LIST_DOMAINS: string[] = ['http://localhost:5173', 'http://localhost:4173', "https://collaborative-platform-frontend.vercel.app/"];
 export const ALLOWED_METHODS: string[] = ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'];
 export const ALLOWED_HEADERS: string[] = ['Content-Type', 'Authorization'];
 
